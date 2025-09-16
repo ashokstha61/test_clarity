b67ad87487f795336307a265c7a97e066ca0fa0f
Ashok Kumar Shrestha <59673063+ashokstha61@users.noreply.github.com>
2025-09-16 11:18:59 +0545

    `Refactored CircularTimerScreen widget, removed unused code and updated functionality`

--- lib/view/sound mixing page/timer_test.dart
+++ lib/view/sound mixing page/timer_test.dart
@@ -3,129 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-// class CircularTimerScreen extends StatefulWidget {
-//   const CircularTimerScreen({super.key});
-
-//   @override
-//   State<CircularTimerScreen> createState() => _CircularTimerScreenState();
-// }
-
-// class _CircularTimerScreenState extends State<CircularTimerScreen> {
-//   final CountDownController _controller = CountDownController();
-//   final int _duration = 10; // 5 minutes (in seconds)
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(backgroundColor: Colors.black),
-//       backgroundColor: Colors.black,
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Text(
-//               'Timer',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: 32,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             const SizedBox(height: 40),
-//             Stack(
-//               alignment: Alignment.center,
-//               children: [
-//                 CircularCountDownTimer(
-//                   duration: _duration,
-//                   initialDuration: 0,
-//                   controller: _controller,
-//                   width: 200,
-//                   height: 200,
-//                   ringColor: Colors.grey[800]!,
-//                   fillColor: Colors.blueAccent,
-//                   backgroundColor: Colors.transparent,
-//                   strokeWidth: 12,
-//                   strokeCap: StrokeCap.round,
-//                   textStyle: const TextStyle(
-//                     fontSize: 48,
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                   textFormat: CountdownTextFormat.HH_MM_SS,
-//                   isReverse: true,
-//                   onComplete: () {
-//                     print('Timer completed!');
-//                   },
-//                 ),
-//                 Positioned(
-//                   top: 40, // Adjust position as needed
-//                   child: Image.asset(
-//                     "assets/images/moon.png",
-//                     width: 60,
-//                     height: 60,
-//                     color: Colors.purple,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             const SizedBox(height: 40),
-//             Spacer(),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 ElevatedButton(
-//                   onPressed: () => _controller.pause(),
-//                   style: ElevatedButton.styleFrom(
-//                     backgroundColor: Colors.grey[800],
-//                     padding: const EdgeInsets.symmetric(
-//                       horizontal: 30,
-//                       vertical: 15,
-//                     ),
-//                   ),
-//                   child: const Text(
-//                     'Pause',
-//                     style: TextStyle(color: Colors.white, fontSize: 18),
-//                   ),
-//                 ),
-//                 const SizedBox(width: 20),
-//                 ElevatedButton(
-//                   onPressed: () => _controller.resume(),
-//                   style: ElevatedButton.styleFrom(
-//                     backgroundColor: Colors.grey[800],
-//                     padding: const EdgeInsets.symmetric(
-//                       horizontal: 30,
-//                       vertical: 15,
-//                     ),
-//                   ),
-//                   child: const Text(
-//                     'Resume',
-//                     style: TextStyle(color: Colors.white, fontSize: 18),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             const SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () => _controller.restart(duration: _duration),
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.red,
-//                 padding: const EdgeInsets.symmetric(
-//                   horizontal: 30,
-//                   vertical: 15,
-//                 ),
-//               ),
-//               child: const Text(
-//                 'Restart',
-//                 style: TextStyle(color: Colors.white, fontSize: 18),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class CircularTimerScreen extends StatefulWidget {
   final int duration;
   final int soundCount; // in seconds
@@ -239,7 +116,9 @@ class _CircularTimerScreenState extends State<CircularTimerScreen> {
                   textFormat: CountdownTextFormat.HH_MM_SS,
                   isReverse: true,
                   onComplete: () {
-                    print('Timer completed!');
+                    setState(() {
+                      _isPaused = true;
+                    });
                   },
                 ),
                 Positioned(
@@ -295,7 +174,7 @@ class _CircularTimerScreenState extends State<CircularTimerScreen> {
                                 ),
                                 SizedBox(height: 4.h),
                                 Text(
-                                  _isPaused ? 'Resume' : 'Pause',
+                                  _isPaused ? 'Play' : 'Pause',
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 14.sp,
@@ -306,7 +185,7 @@ class _CircularTimerScreenState extends State<CircularTimerScreen> {
                           ),
                           SizedBox(width: 20.w),
                           IconButton(
-                            onPressed: () => Navigator.pop(context),
+                            onPressed: () => _controller.reset(),
                             icon: Column(
                               children: [
                                 Image.asset(
